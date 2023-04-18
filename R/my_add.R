ea30749ef39d9ec5936404b92978809f15d4bc14
Mageese <maggdong20@gmail.com>
2023-04-17 21:05:42 -0700

    my_add function

---
+++ R/my_add.R
@@ -0,0 +1,15 @@
+#' Add two parameters
+#'
+#' @param x An integer
+#' @param y An integer defaulted at 10
+#'
+#' @return An integer.
+#' @export
+#'
+#' @examples
+#' x <- 2
+#' y <- 3
+#' my_add(x,y)
+my_add <- function(x,y=10){
+  x + y
+}
